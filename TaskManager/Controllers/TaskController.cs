e1a4612eca2633f7688e36240cd48e1f75b82b62
Xavier Xie <xieyuehao@gmail.com>
2018-11-09 23:33:55 +1000

    inti

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/Task")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        // GET: api/Task
+        [HttpGet]
+        public IEnumerable<Models.Task> Get()
+        {
+            return new Models.Task[] {
+                new Models.Task() { id= 1, name= "task1" , startDate =DateTime.Today.AddMonths(-1), endDate = DateTime.Today.AddMonths(2), status = "progress", assignedTo=1},
+                new Models.Task() { id= 2, name= "task2" , startDate =DateTime.Today.AddMonths(-1), endDate = DateTime.Today.AddMonths(2), status = "progress", assignedTo=1},
+            };
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Task/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Task
+    { 
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int assignedTo { get; set; }
+        public string status { get; set; }
+    }
+}
